8b82add4a2d9c6a6268f05f3df382e9b2f6634f2
Conrad Beach <conradbeach@protonmail.com>
2019-09-26 14:10:07 -0600

    Various improvements to Mac setup script
    I noticed a number of opportunities for improvement when I set up my new laptop. There's still issues, but I fixed a number of existing issues.

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Brew
-echo "** Installing Brew **"
+echo "\n** Installing Brew **"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 ## Packages
-echo "** Installing Brew Packages **"
+echo "\n** Installing Brew Packages **"
 brew install ack
 brew install asciinema
 brew install bat
@@ -21,8 +21,6 @@ brew tap heroku/brew && brew install heroku
 brew install htop
 brew install mas
 brew install neovim
-# neovim-dot-app: A full installation of Xcode.app is required to compile this software. Installing just the Command Line Tools is not sufficient.
-brew tap rogual/neovim-dot-app && brew install neovim-dot-app
 brew install pinentry-mac
 brew install python
 brew install python@2
@@ -38,105 +36,18 @@ brew install yarn
 brew install zsh
 
 ## Fonts
-echo "** Installing Brew Fonts **"
+echo "\n** Installing Brew Fonts **"
 brew tap caskroom/fonts
 brew cask install font-hack-nerd-font
 
 ## Services
-echo "** Installing Brew Services **"
+echo "\n** Installing Brew Services **"
 brew install postgresql
 brew services start postgresql
 
 
-# asdf
-echo "** Installing asdf **"
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.0
-
-## Ruby
-echo "** Installing Ruby **"
-xcode-select --install
-brew install openssl libyaml libffi
-asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
-asdf install ruby 2.5.5
-
-## Erlang
-echo "** Installing Erlang **"
-brew install autoconf wxmac
-asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
-
-## Elixir
-echo "** Installing Elixir **"
-asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
-
-## NodeJS
-echo "** Installing NodeJS **"
-brew install coreutils gpg
-asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
-asdf install nodejs 12.3.1
-
-# Zsh
-
-## Oh My Zsh
-echo "** Installing Oh My Zsh **"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-## zsh-git-prompt
-echo "** Installing zsh-git-prompt **"
-mkdir ~/.oh-my-zsh/zsh-git-prompt
-curl https://raw.githubusercontent.com/olivierverdier/zsh-git-prompt/master/zshrc.sh -o ~/.oh-my-zsh/zsh-git-prompt/zshrc.sh
-
-## zsh-syntax-highlighting
-echo "** Installing zsh-syntax-highlighting **"
-brew install zsh-syntax-highlighting
-
-## zsh-completions
-echo "** Installing zsh-completions **"
-brew install zsh-completions
-
-# Neovim
-
-## vim-plug
-echo "** Installing vim-plug **"
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-## Python Support
-echo "** Installing Neovim Modules for Python **"
-pip2 install neovim
-pip3 install neovim
-
-# Other Tools
-
-## rupa/z
-echo "** Installing z **"
-curl https://raw.githubusercontent.com/rupa/z/master/z.sh -o ~/.oh-my-zsh/z.sh
-
-## Tmuxinator
-echo "** Installing Tmuxinator **"
-curl https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.zsh -o ~/.tmuxinator.zsh
-
-## tat
-echo "** Installing tat **"
-curl https://raw.githubusercontent.com/thoughtbot/dotfiles/master/bin/tat -o /usr/local/bin/tat && chmod +x /usr/local/bin/tat
-
-## Tmux Plugin Manager
-echo "** Installing Tmux Plugin Manager **"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-## vim-anywhere
-echo "** Installing vim-anywhere **"
-curl -fsSL https://raw.github.com/cknadler/vim-anywhere/master/install | bash
-echo '/usr/local/opt/neovim-dot-app/bin/gnvim' > ~/.vim-anywhere/.path
-
-## Other Other
-echo "** Installing Other Various Tools **"
-yarn global add emoj
-yarn global add pure-prompt
-yarn global add neovim
-
-
 # Link Files
-echo "** Symlinking Files **"
+echo "\n** Symlinking Files **"
 ln -s ~/Development/dotfiles/nvim ~/.config
 ln -s ~/Development/dotfiles/ssh/config ~/.ssh
 rm ~/.ssh/known_hosts
@@ -169,30 +80,112 @@ mkdir ~/.gnupg/
 ln -s ~/Development/dotfiles/gpg-agent.conf ~/.gnupg/
 
 
+# asdf
+echo "\n** Installing asdf **"
+rm -rf ~/.asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.4
+source ~/.zshrc
+
+## Ruby
+echo "\n** Installing Ruby **"
+xcode-select --install
+brew install openssl libyaml libffi
+asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+
+## Erlang
+echo "\n** Installing Erlang **"
+brew install autoconf wxmac
+asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
+
+## Elixir
+echo "\n** Installing Elixir **"
+asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
+
+## NodeJS
+echo "\n** Installing NodeJS **"
+brew install coreutils gpg
+asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+
+## Install Global Versions
+echo "\n** Installing All Global Versions of Languages **"
+asdf install
+
+
+# Zsh
+
+## Oh My Zsh
+echo "\n** Installing Oh My Zsh **"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+## zsh-git-prompt
+echo "\n** Installing zsh-git-prompt **"
+mkdir ~/.oh-my-zsh/zsh-git-prompt
+curl https://raw.githubusercontent.com/olivierverdier/zsh-git-prompt/master/zshrc.sh -o ~/.oh-my-zsh/zsh-git-prompt/zshrc.sh
+
+## zsh-syntax-highlighting
+echo "\n** Installing zsh-syntax-highlighting **"
+brew install zsh-syntax-highlighting
+
+## zsh-completions
+echo "\n** Installing zsh-completions **"
+brew install zsh-completions
+
+# Neovim
+
+## vim-plug
+echo "\n** Installing vim-plug **"
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+## Python Support
+echo "\n** Installing Neovim Modules for Python **"
+pip2 install neovim
+pip3 install neovim
+
+# Other Tools
+
+## rupa/z
+echo "\n** Installing z **"
+curl https://raw.githubusercontent.com/rupa/z/master/z.sh -o ~/.oh-my-zsh/z.sh
+
+## Tmuxinator
+echo "\n** Installing Tmuxinator **"
+curl https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.zsh -o ~/.tmuxinator.zsh
+
+## tat
+echo "\n** Installing tat **"
+curl https://raw.githubusercontent.com/thoughtbot/dotfiles/master/bin/tat -o /usr/local/bin/tat && chmod +x /usr/local/bin/tat
+
+## Tmux Plugin Manager
+echo "\n** Installing Tmux Plugin Manager **"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+## vim-anywhere
+echo "\n** Installing vim-anywhere **"
+brew install macvim
+curl -fsSL https://raw.github.com/cknadler/vim-anywhere/master/install | bash
+echo '/usr/local/opt/neovim-dot-app/bin/gnvim' > ~/.vim-anywhere/.path
+
+## Other Other
+echo "\n** Installing Other Various Tools **"
+yarn global add emoj
+yarn global add pure-prompt
+yarn global add neovim
+
+
+
 # Apps
-echo ""
-echo ""
-echo "TODO: Run :checkhealth in Neovim."
-echo ""
-echo "TODO: Run :PlugInstall in Neovim."
-echo ""
-echo "TODO: Hit <prefix>+I in tmux to install plugins."
-echo ""
-echo "TODO: Set vim-anywhere keybinding to Command-Option-V. (System Preferences > Keyboard > Shortcuts > Services > Vim Anywhere)"
-echo ""
-echo "TODO: Symlink dotfiles/gitkraken/profile to ~/.gitkraken/profiles/???/"
-echo ""
-echo "TODO: Copy ~/.atom config folder from Time Machine backup onto this machine."
-echo ""
-echo "TODO: Copy ~/Development folder from Time Machine backup onto this machine."
-echo ""
-echo "TODO: Check if there are any other files you should copy from Time Machine. Shouldn't be."
-echo ""
-echo "TODO: Turn on FileVault. Go through other MacOS settings as well."
-echo ""
-echo "TODO: Install the following softwares."
+echo "\n\nTODO: Run :checkhealth in Neovim."
+echo "\nTODO: Run :PlugInstall in Neovim."
+echo "\nTODO: Hit <prefix>+I in tmux to install plugins."
+echo "\nTODO: Set vim-anywhere keybinding to Command-Option-V. (System Preferences > Keyboard > Shortcuts > Services > Vim Anywhere)"
+echo "\nTODO: Symlink dotfiles/gitkraken/profile to ~/.gitkraken/profiles/???/"
+echo "\nTODO: Copy ~/.atom config folder from Time Machine backup onto this machine."
+echo "\nTODO: Copy ~/Development folder from Time Machine backup onto this machine."
+echo "\nTODO: Check if there are any other files you should copy from Time Machine. Shouldn't be."
+echo "\nTODO: Turn on FileVault. Go through other MacOS settings as well."
+echo "\n\nTODO: Install the following softwares."
 echo "**************************************"
-echo "* [ ] Neovim.app"
 echo "* [ ] Atom"
 echo "* [ ] Balena Etcher"
 echo "* [ ] BetterTouchTool"
@@ -225,7 +218,7 @@ echo "* [ ] Kindle"
 echo "* [ ] Lacona"
 echo "    * Change keybinding to command-space."
 echo "    * Change Spotlight keybinding to option-space. (In Settings > Keyboard > Shortcuts > Spotlight"
-echo "    * Install addons: Caffeniate, Convert Currency, GitHub, Shell Commands, Timers & Alarms."
+echo "    * Install addons: Caffeniate, Convert Currency, GitHub, Shell Commands, Time Zones, Timers & Alarms."
 echo "* [ ] Noizio (Mac App Store)"
 echo "* [ ] Numi"
 echo "* [ ] Oni"
@@ -235,7 +228,7 @@ echo "    * Login to get saved API calls."
 echo "* [ ] ProtonVPN"
 echo "* [ ] Slack"
 echo "* [ ] Snip (Mac App Store)"
-echo "    * Set shortcut to command-control-a"
+echo "    * Set shortcut to command-control-n"
 echo "* [ ] Sophos Home (Log into your dashboard to install.)"
 echo "* [ ] Station"
 echo "    * Gmail (Atomic Jolt)"
@@ -251,9 +244,10 @@ echo "* [ ] TablePlus"
 echo "* [ ] Terminal.app"
 echo "    * Load theme."
 echo "    * In Settings > General, set custom theme to be the default."
-echo "    * In Settings > General, change 'Shells open with:' to 'bin/zsh'."
+echo "    * In Settings > General, change 'Shells open with:' to '/bin/zsh'."
 echo "* [ ] VirtualBox"
 echo "    * Install Extension Pack"
 echo "* [ ] Visual Studio Code"
 echo "* [ ] VLC"
 echo "* [ ] Xcode (Just install the full application even though it's massive.)"
+echo "* [ ] brew tap rogual/neovim-dot-app && brew install neovim-dot-app" # Requires Xcode
