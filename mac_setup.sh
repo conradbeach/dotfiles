19b86b8d0efae86d473246f2815234535da4fc4d
Conrad Beach <conradbeach@protonmail.com>
2020-08-15 08:11:37 -0600

    Remove Python2 package install
    Python2 is officially retired.

--- mac_setup.sh
+++ mac_setup.sh
@@ -200,9 +200,8 @@ echo "\n** Installing vim-plug **"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ## Python Support
-echo "\n** Installing Neovim Modules for Python **"
-pip2 install neovim
-pip3 install neovim
+echo "\n** Installing Neovim Package for Python **"
+pip install neovim
 
 # Other Tools
 
