2da99ee9e13a6bccc5202286b8e7e5ba27309f24
Conrad Beach <conrad.beach@simplepractice.com>
2021-12-20 09:50:12 -0700

    Update vim-plug install command
    This is the command currently given in the README [1].
    
    [1]: https://github.com/junegunn/vim-plug

--- mac_setup.sh
+++ mac_setup.sh
@@ -185,7 +185,7 @@ brew install zsh-completions
 
 ## vim-plug
 print_header "Installing vim-plug"
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 ## Python Support
 print_header "Installing Neovim Package for Python"
