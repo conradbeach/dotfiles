08a71edd0a077a325e3584104fed36e177fd8b34
Conrad Beach <conrad.beach@simplepractice.com>
2022-11-17 09:44:40 -0700

    Add leap.nvim plugin
    https://github.com/ggandor/leap.nvim

---
+++ nvim/config/plugins/leap-dot-nvim.lua
@@ -0,0 +1,3 @@
+vim.keymap.set({'n', 'x', 'o'}, '<Leader>s', '<Plug>(leap-forward-to)')
+vim.keymap.set({'n', 'x', 'o'}, '<Leader>S', '<Plug>(leap-backward-to)')
+vim.keymap.set({'n', 'x', 'o'}, '<Leader>ws', '<Plug>(leap-cross-window)')
