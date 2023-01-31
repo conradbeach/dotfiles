8e6b4c13f7de178bc5c124a0a6c1067bef0fed4f
Conrad Beach <conrad.beach@simplepractice.com>
2023-01-31 13:45:13 -0700

    Improve LunarVim keymappings

--- lunarvim/config.lua
+++ lunarvim/config.lua
@@ -17,22 +17,40 @@ lvim.builtin.bufferline.options = {
 vim.opt["textwidth"] = 80
 vim.opt["colorcolumn"] = "+1"
 
-
 -- # Keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 
--- ## Tab Switching
+-- 0 goes to the first non-whitespace character.
+lvim.keys.normal_mode["0"] = "^"
+
+-- Allow moving through wrapped lines.
+lvim.keys.normal_mode["k"] = "gk"
+lvim.keys.normal_mode["j"] = "gj"
+
+lvim.builtin.which_key.mappings["nh"] = { "<cmd>noh<cr>", "Clear Search Highlights" }
+
+lvim.builtin.which_key.mappings["z"] = { "<cmd>wincmd _<cr><cmd>wincmd |<cr>", "Zoom Pane" }
+
+lvim.builtin.which_key.mappings["t"] = {
+  name = "Tabs",
+  n = { "<cmd>tabnew<cr>", "New Tab"},
+  c = { "<cmd>tabclose<cr>", "Close Tab"},
+  C = { "<cmd>tabonly<cr>", "Close Other Tabs"},
+  W = { "<cmd>on<cr>", "Close Other Windows"},
+  E = { "<cmd>on<cr><cmd>tabonly<cr>", "Close Other Tabs & Windows"},
+}
+-- Navigating tabs
 lvim.builtin.which_key.mappings["h"] = { "gT", "Previous Tab" }
 lvim.builtin.which_key.mappings["l"] = { "gt", "Next Tab" }
-lvim.builtin.which_key.mappings["1"] = { "1gt", "Tab 1" }
-lvim.builtin.which_key.mappings["2"] = { "2gt", "Tab 2" }
-lvim.builtin.which_key.mappings["3"] = { "3gt", "Tab 3" }
-lvim.builtin.which_key.mappings["4"] = { "4gt", "Tab 4" }
-lvim.builtin.which_key.mappings["5"] = { "5gt", "Tab 5" }
-lvim.builtin.which_key.mappings["6"] = { "6gt", "Tab 6" }
-lvim.builtin.which_key.mappings["7"] = { "7gt", "Tab 7" }
-lvim.builtin.which_key.mappings["8"] = { "8gt", "Tab 8" }
-lvim.builtin.which_key.mappings["9"] = { "9gt", "Tab 9" }
+lvim.builtin.which_key.mappings["1"] = { "1gt", "Go to Tab 1" }
+lvim.builtin.which_key.mappings["2"] = { "2gt", "Go to Tab 2" }
+lvim.builtin.which_key.mappings["3"] = { "3gt", "Go to Tab 3" }
+lvim.builtin.which_key.mappings["4"] = { "4gt", "Go to Tab 4" }
+lvim.builtin.which_key.mappings["5"] = { "5gt", "Go to Tab 5" }
+lvim.builtin.which_key.mappings["6"] = { "6gt", "Go to Tab 6" }
+lvim.builtin.which_key.mappings["7"] = { "7gt", "Go to Tab 7" }
+lvim.builtin.which_key.mappings["8"] = { "8gt", "Go to Tab 8" }
+lvim.builtin.which_key.mappings["9"] = { "9gt", "Go to Tab 9" }
 -- Switch to last active tab.
 vim.api.nvim_create_autocmd("TabLeave",  {
     pattern = "*",
