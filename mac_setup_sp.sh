55e85999cf2837709868ce131c9089803b0f6464
Conrad Beach <conrad.beach@simplepractice.com>
2023-01-31 08:54:56 -0700

    Add missing steps for LunarVim

--- mac_setup_sp.sh
+++ mac_setup_sp.sh
@@ -18,6 +18,7 @@ sudo softwareupdate --install-rosetta
 print_header "Symlinking Files"
 mkdir ~/.config
 ln -sf ~/Development/dotfiles/nvim ~/.config
+ln -sf ~/Development/dotfiles/lunarvim/config.lua ~/.config/lvim
 mkdir ~/.ssh/
 ln -sf ~/Development/dotfiles/ssh/config ~/.ssh/
 ln -sf ~/Development/dotfiles/tmuxinator ~/.config
@@ -138,11 +139,8 @@ print_header "Installing zsh-completions"
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 
 
-# Neovim
-
-## vim-plug
-print_header "Installing vim-plug"
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# Neovim/LunarVim
+LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh) --yes
 
 
 # Other Tools
