09b15433064da89e11d5725418869ce0dbbc9402
Conrad Beach <conrad.beach@simplepractice.com>
2023-02-08 14:52:11 -0700

    Fix link command in SP setup script
    I forgot I need to rename the link.

--- mac_setup_sp.sh
+++ mac_setup_sp.sh
@@ -40,7 +40,7 @@ ln -sf ~/Development/dotfiles/.tmux.conf ~
 ln -sf ~/Development/dotfiles/.tool-versions ~
 ln -sf ~/Development/dotfiles/.vimrc ~ 
 ln -sf ~/Development/dotfiles/.zshrc ~
-ln -sf ~/Development/dotfiles/git-commit-template-sp.txt ~
+ln -sf ~/Development/dotfiles/git-commit-template-sp.txt ~/git-commit-template.txt
 # gpg-agent.conf is linked later on this script because gnupg sets up the ~/.gnupg directory
 # in a certain way, so I need to link this file after gnupg runs once.
 
