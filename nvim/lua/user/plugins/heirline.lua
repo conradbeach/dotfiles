48229744883a8a929e77d1a61c7e7bf4a3b9ffcf
Conrad Beach <conrad.beach@simplepractice.com>
2023-04-13 16:01:40 -0600

    Improve comments

--- nvim/lua/user/plugins/heirline.lua
+++ nvim/lua/user/plugins/heirline.lua
@@ -3,14 +3,15 @@ return {
   opts = function(_, opts)
     local status = require("astronvim.utils.status")
 
+    -- Modifying `statusline` table: https://github.com/AstroNvim/AstroNvim/blob/main/lua/plugins/heirline.lua#L15
     opts.statusline[3] = status.component.file_info {
-      filename = { modify = ":." },
+      filename = { modify = ":." }, -- Shows full file path relative to working directory.
     }
     opts.statusline[12] = nil -- Remove mode indicator at the end of the status line
 
-    opts.tabline = nil -- remove tabline
+    opts.tabline = nil -- Remove tabline
 
-    -- return the final configuration table
+    -- Return the final configuration table
     return opts
   end,
 }
