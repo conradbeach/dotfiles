2b99dd3e10d81d621e9ba17264ce7e3a4743f734
Conrad Beach <conrad.beach@simplepractice.com>
2023-04-14 06:32:28 -0600

    Move Spectre config to a separate file
    It's big enough that it seems worth it to move it to its own file.
    
    [skip ci] # Remove this if you actually want to run a build.

---
+++ nvim/lua/user/plugins/spectre.lua
@@ -0,0 +1,44 @@
+return {
+  "nvim-pack/nvim-spectre",
+  config = function()
+    require("spectre").setup({
+      mapping={
+        ['send_to_qf'] = {
+            map = "<leader>fsq",
+            cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
+            desc = "send all item to quickfix"
+        },
+        ['replace_cmd'] = {
+            map = "<leader>fsc",
+            cmd = "<cmd>lua require('spectre.actions').replace_cmd()<CR>",
+            desc = "input replace vim command"
+        },
+        ['show_option_menu'] = {
+            map = "<leader>fso",
+            cmd = "<cmd>lua require('spectre').show_options()<CR>",
+            desc = "show option"
+        },
+        ['run_current_replace'] = {
+          map = "<leader>fsr",
+          cmd = "<cmd>lua require('spectre.actions').run_current_replace()<CR>",
+          desc = "replace current line"
+        },
+        ['run_replace'] = {
+            map = "<leader>fsR",
+            cmd = "<cmd>lua require('spectre.actions').run_replace()<CR>",
+            desc = "replace all"
+        },
+        ['change_view_mode'] = {
+            map = "<leader>fsv",
+            cmd = "<cmd>lua require('spectre').change_view()<CR>",
+            desc = "change result view mode"
+        },
+        ['resume_last_search'] = {
+          map = "<leader>fsl",
+          cmd = "<cmd>lua require('spectre').resume_last_search()<CR>",
+          desc = "resume last search before close"
+        },
+      },
+     })
+  end,
+}
