70be2e0e8d361d7904fcbaa5c98a5459baf09556
Conrad Beach <conrad.beach@simplepractice.com>
2023-04-25 16:11:29 -0600

    Clean up AstroNvim config files
    Remove unhelpful comments, improved style, removed
    unnecessary config values.

--- nvim/lua/user/mappings.lua
+++ nvim/lua/user/mappings.lua
@@ -1,8 +1,8 @@
 -- Mapping data with "desc" stored directly by vim.keymap.set().
 --
 -- Please use this mappings table to set keyboard mapping since this is the
--- lower level configuration and more robust one. (which-key will
--- automatically pick-up stored data by this setting.)
+-- lower level configuration and more robust one.
+-- (which-key will automatically pick-up stored data by this setting.)
 return {
   -- first key is the mode
   n = {
@@ -18,8 +18,6 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     -- ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
 
     -- Remove some mappings I don't want.
     ["<leader>c"] = false, -- Close buffer. I don't worry about buffers.
