aa705e433532e0d77a5b70b83b212965f2da5b29
Conrad Beach <conrad.beach@simplepractice.com>
2023-07-10 14:54:21 -0700

    Update Treesitter config in AstroNvim

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -9,5 +9,6 @@ return {
       "lua",
       "ruby",
     },
+    indent = { enable = false }, -- I was having some annoying issues with incorrect indentation; turning this off seems to have resolved it.
   },
 }
