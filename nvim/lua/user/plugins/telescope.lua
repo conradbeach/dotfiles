ced53eb989ca29f74bd674b4b8203331db1a20cb
Conrad Beach <conrad.beach@simplepractice.com>
2023-08-31 13:56:49 -0600

    Exclude VCR cassettes from primary file finder
    I'm almost never interested in finding a cassette and they clutter up
    searches. In fact, they usually appear at the top because they don't
    have underscores in the filename, so they match a search of `planscontroller`
    better than `plans_controller.rb` so I have to scroll through a few
    cassette files before getting to the file I actually want.
    
    In the event that I actually want to look for a given cassette, I added a
    specialized finder for it.

--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -25,7 +25,7 @@ return {
     opts.pickers = {
       find_files = {
         -- `hidden = true` will still show files inside of `.git/` as it's not `.gitignore`d.
-        find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*" },
+        find_command = { "rg", "--files", "--hidden", "--glob", "!.git/*", "--glob", "!spec/fixtures/vcr_cassettes/**" },
       },
     }
 
