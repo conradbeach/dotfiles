3edc27fb1e361a853a2f7950c7a1a28756810977
Conrad Beach <conrad.beach@simplepractice.com>
2023-09-08 13:57:33 -0600

    Add scrolloff config back in to Neovim
    It was recently removed [1] from AstroNvim.
    
    [1]: https://github.com/AstroNvim/AstroNvim/commit/e711b6fd6d319efe70f5175f582a04b43d6c5c3a

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,9 +1,11 @@
 -- set vim options here (vim.<first_key>.<second_key> = value)
 return {
   opt = {
-    wrap = true, -- sets vim.opt.wrap
-    textwidth = 80, -- Make it obvious where 80 characters is.
     colorcolumn = "+1", -- Make it obvious where 80 characters is.
+    scrolloff = 8, -- number of lines to keep above and below the cursor
+    sidescrolloff = 8, -- number of columns to keep at the sides of the cursor
+    textwidth = 80, -- Make it obvious where 80 characters is.
+    wrap = true, -- sets vim.opt.wrap
   },
   g = {
     autoformat_enabled = false, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
