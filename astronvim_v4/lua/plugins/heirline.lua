c2afa59153a7af174aae36c4d0518bfaaf04c666
Conrad Beach <cbeach@deseretbook.com>
2024-07-16 15:36:40 -0600

    Update status line

--- astronvim_v4/lua/plugins/heirline.lua
+++ astronvim_v4/lua/plugins/heirline.lua
@@ -40,15 +40,15 @@ return {
       },
     }
 
-    -- Modifying `statusline` table: https://github.com/AstroNvim/AstroNvim/blob/main/lua/plugins/heirline.lua#L15
+    -- Modifying `statusline` table: https://github.com/AstroNvim/AstroNvim/blob/main/lua/astronvim/plugins/heirline.lua#L58
     opts.statusline[3] = status.component.file_info {
       filename = { modify = ":." }, -- Shows full file path relative to working directory.
+      filetype = false
     }
-    opts.statusline[12] = nil -- Remove mode indicator at the end of the status line
+    opts.statusline[13] = nil -- Remove mode indicator at the end of the status line
 
     opts.tabline = nil -- Remove tabline; using `akinsho/bufferline.nvim` instead.
 
-    -- Return the final configuration table
     return opts
   end,
 }
