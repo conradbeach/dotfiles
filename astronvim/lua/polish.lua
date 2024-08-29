db526d17a6c45986129e171fc543b8563bce46a4
Conrad Beach <cbeach@deseretbook.com>
2024-08-29 13:27:14 -0600

    Disable focus.nvim for certain file types

--- astronvim/lua/polish.lua
+++ astronvim/lua/polish.lua
@@ -6,30 +6,46 @@
 -- there are new `autocmds` and `commands` keys that can be used for the below configuration.
 
 -- Switch to last active tab.
-vim.api.nvim_create_autocmd("TabLeave",  {
-    pattern = "*",
-    callback = function()
-        vim.api.nvim_set_keymap(
-          'n',
-          '<Leader>0',
-          '<cmd>tabn ' .. vim.api.nvim_tabpage_get_number(0) .. '<CR>',
-          { noremap = true, silent = true }
-        )
+vim.api.nvim_create_autocmd("TabLeave", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_set_keymap(
+      "n",
+      "<Leader>0",
+      "<cmd>tabn " .. vim.api.nvim_tabpage_get_number(0) .. "<CR>",
+      { noremap = true, silent = true }
+    )
+  end,
+})
+
+-- nvim-focus/focus.nvim
+local ignore_filetypes = { 'NvimTree', 'Avante' }
+local augroup = vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup,
+  callback = function(_)
+    if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+      vim.b.focus_disable = true
+    else
+      vim.b.focus_disable = false
     end
+  end,
+  desc = "Disable focus autoresize for FileType",
 })
 
 -- vim-test/vim-test
-vim.cmd("let test#strategy = 'toggleterm'")
-vim.cmd("let test#ruby#rspec#executable = 'bundle exec rspec --format=documentation'")
+vim.cmd "let test#strategy = 'toggleterm'"
+vim.cmd "let test#ruby#rspec#executable = 'bundle exec rspec --format=documentation'"
 
 -- codota/tabnine-nvim
-vim.api.nvim_command("TabnineDisable") -- I only want chat.
+vim.api.nvim_command "TabnineDisable" -- I only want chat.
 
 -- tpope/vim-rails
 -- Open the alternate file in a vertical split even if it doesn't exist.
-vim.cmd("command AC :vsplit | execute 'e ' . eval('rails#buffer().alternate()')")
+vim.cmd "command AC :vsplit | execute 'e ' . eval('rails#buffer().alternate()')"
 
 -- AndrewRadev/splitjoin.vim
 -- Adding these to the mappings table didn't work for some reason.
-vim.api.nvim_set_keymap('n', 'gJ', '<cmd>silent SplitjoinJoin<CR>', {})
-vim.api.nvim_set_keymap('n', 'gS', '<cmd>silent SplitjoinSplit<CR>', {})
+vim.api.nvim_set_keymap("n", "gJ", "<cmd>silent SplitjoinJoin<CR>", {})
+vim.api.nvim_set_keymap("n", "gS", "<cmd>silent SplitjoinSplit<CR>", {})
