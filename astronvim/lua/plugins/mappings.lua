4d06d34db460518632a392e25ec443fe3d911c73
Conrad Beach <cbeach@deseretbook.com>
2024-10-02 11:43:56 -0600

    Add mappings for finding the word under the cursor

--- astronvim/lua/plugins/mappings.lua
+++ astronvim/lua/plugins/mappings.lua
@@ -97,6 +97,10 @@ return {
 
           -- nvim-telescope/telescope.nvim
           ["<Leader>fc"] = { "<cmd>lua require('telescope-live-grep-args.shortcuts').grep_word_under_cursor()<cr>", desc = "Find word under cursor" },
+          ["<Leader>fi"] = { "", desc = "Find word under cursor in..." },
+          ["<Leader>fib"] = { "<cmd>lua require('telescope-live-grep-args.shortcuts').grep_word_under_cursor_current_buffer()<cr>", desc = "Find word under cursor in current buffer" },
+          ["<Leader>fia"] = { "<cmd>lua require('telescope-live-grep-args.shortcuts').grep_word_under_cursor({ search_dirs = { 'app' } })<cr>", desc = "Find word under cursor in `app` directory" },
+          ["<Leader>fis"] = { "<cmd>lua require('telescope-live-grep-args.shortcuts').grep_word_under_cursor({ search_dirs = { 'spec' } })<cr>", desc = "Find word under cursor in `spec` directory" },
           ["<Leader>fp"] = {
             function() require("telescope.builtin").find_files { find_command = { "rg", "--files", "--glob", "spec/fixtures/vcr_cassettes/**", "--glob", "**/__snapshots__/**" } } end,
             desc = "Find snapshots",
