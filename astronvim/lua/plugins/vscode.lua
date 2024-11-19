3b118597cefb0f2c3ce6237458ee5ab32e1d45ec
Conrad Beach <cbeach@deseretbook.com>
2024-11-19 15:05:17 -0700

    Remove vim-rails from VS Code Neovim
    It kind of worked, but it caused problems.

--- astronvim/lua/plugins/vscode.lua
+++ astronvim/lua/plugins/vscode.lua
@@ -17,7 +17,6 @@ vim.tbl_map(function(plugin) enabled[plugin] = true end, {
   "nvim-treesitter-textobjects",
   "nvim-ts-context-commentstring",
   "splitjoin.vim",
-  "vim-rails",
   "vim-surround",
   "vim-swap",
   "vim-repeat",
