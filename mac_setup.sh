a5e4f6c0262fed1c99bd54d426b811c59822ccd5
Conrad Beach <cbeach@deseretbook.com>
2025-01-07 08:29:48 -0700

    Don't install ChatGPT app
    I use Perplexity and Cherry Studio now.

--- mac_setup.sh
+++ mac_setup.sh
@@ -106,7 +106,6 @@ brew install --cask arc
 if ! is_deseret_book; then brew install --cask balenaetcher; fi
 brew install --cask bettertouchtool
 brew install --cask bitwarden
-brew install --cask chatgpt
 brew install --cask dash
 brew install --cask firefox@developer-edition
 if ! is_deseret_book; then brew install --cask gimp; fi
