d6932d1bb3f725c85cef08ba0b2ff3ca68cb0362
Conrad Beach <cbeach@deseretbook.com>
2025-04-01 10:32:51 -0600

    Move snacks.nvim config to a separate file

---
+++ astronvim/lua/plugins/snacks.lua
@@ -0,0 +1,43 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = {
+      preset = {
+        header = table.concat({
+          " █████  ███████ ████████ ██████   ██████ ",
+          "██   ██ ██         ██    ██   ██ ██    ██",
+          "███████ ███████    ██    ██████  ██    ██",
+          "██   ██      ██    ██    ██   ██ ██    ██",
+          "██   ██ ███████    ██    ██   ██  ██████ ",
+          "",
+          "███    ██ ██    ██ ██ ███    ███",
+          "████   ██ ██    ██ ██ ████  ████",
+          "██ ██  ██ ██    ██ ██ ██ ████ ██",
+          "██  ██ ██  ██  ██  ██ ██  ██  ██",
+          "██   ████   ████   ██ ██      ██",
+          "                                ",
+          "       What would Jesus do?     ",
+        }, "\n"),
+      },
+    },
+    picker = {
+      formatters = {
+        file = {
+          truncate = 1000, -- truncate the file path to (roughly) this length
+        },
+      },
+    },
+  },
+  dependencies = {
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          n = {
+            ["<Leader>fu"] = { function() require("snacks").picker.undo() end, desc = "Undo history" },
+          },
+        },
+      },
+    },
+  },
+}
--- astronvim/lua/plugins/user.lua
+++ astronvim/lua/plugins/user.lua
@@ -129,49 +129,6 @@ return {
     end,
   },
 
-  {
-    "folke/snacks.nvim",
-    opts = {
-      dashboard = {
-        preset = {
-          header = table.concat({
-            " █████  ███████ ████████ ██████   ██████ ",
-            "██   ██ ██         ██    ██   ██ ██    ██",
-            "███████ ███████    ██    ██████  ██    ██",
-            "██   ██      ██    ██    ██   ██ ██    ██",
-            "██   ██ ███████    ██    ██   ██  ██████ ",
-            "",
-            "███    ██ ██    ██ ██ ███    ███",
-            "████   ██ ██    ██ ██ ████  ████",
-            "██ ██  ██ ██    ██ ██ ██ ████ ██",
-            "██  ██ ██  ██  ██  ██ ██  ██  ██",
-            "██   ████   ████   ██ ██      ██",
-            "                                ",
-            "       What would Jesus do?     ",
-          }, "\n"),
-        },
-      },
-      picker = {
-        formatters = {
-          file = {
-            truncate = 1000, -- truncate the file path to (roughly) this length
-          },
-        },
-      },
-    },
-    dependencies = {
-      {
-        "AstroNvim/astrocore",
-        opts = {
-          mappings = {
-            n = {
-              ["<Leader>fu"] = { function() require("snacks").picker.undo() end, desc = "Undo history" },
-            },
-          },
-        },
-      },
-    },
-  },
 
   { "AndrewRadev/splitjoin.vim", lazy = false },
 
