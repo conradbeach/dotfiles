3afb676093923c4c738513de8bab2f3655be5be0
Conrad Beach <cbeach@deseretbook.com>
2025-04-15 08:24:07 -0600

    Simplify Avante configuration
    By specifying Haiku as the provider, Avente automatically selects some
    configuration values that fit Haiku. This should fix some errors I was
    having and lead to a better experience.

--- astronvim/lua/plugins/avante.lua
+++ astronvim/lua/plugins/avante.lua
@@ -40,14 +40,9 @@ return {
       },
     },
     opts = {
+      provider = "claude-haiku",
       behaviour = {
         support_paste_from_clipboard = false,
-        enable_claude_text_editor_tool_mode = false,
-      },
-      claude = {
-        model = "claude-3-5-haiku-latest",
-        -- model = "claude-3-7-sonnet-latest",
-        disable_tools = true
       },
       mappings = {
         diff = {
