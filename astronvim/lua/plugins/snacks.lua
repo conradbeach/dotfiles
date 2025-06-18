c1e50dd7c0683144dc739c227b728d3a5be401a5
Conrad Beach <cbeach@deseretbook.com>
2025-06-18 13:34:46 -0600

    Improve Snacks.nvim config
    Rather than overriding built in maps, I can change the Snacks config
    to make the built in maps work the way I want.
    
    This still isn't working entirely correctly though. If I open the file picker
    in a git worktree (`<Leader>ff`), it does not include hidden files. You
    can hit option-h to include hidden files, but I don't know why it doesn't
    just work. It works in non-worktree repos.

--- astronvim/lua/plugins/snacks.lua
+++ astronvim/lua/plugins/snacks.lua
@@ -1,24 +1,5 @@
 return {
   "folke/snacks.nvim",
-  dependencies = {
-    {
-      "AstroNvim/astrocore",
-      opts = {
-        mappings = {
-          n = {
-            -- The following mappings are provided by AstroNvim but they don't include hidden files which I want.
-            ["<Leader>fw"] = {
-              function() require("snacks").picker.grep { hidden = true } end, desc = "Find words",
-            },
-            ["<Leader>fc"] = {
-              function() require("snacks").picker.grep_word { hidden = true } end, desc = "Find word under cursor",
-            },
-            ["<Leader>fo"] = { function() require("snacks").picker.recent { hidden = true } end, desc = "Find old files" },
-          },
-        },
-      },
-    },
-  },
   opts = {
     dashboard = {
       preset = {
@@ -40,12 +21,31 @@ return {
       },
     },
     picker = {
+      exclude = { -- Folder names to exclude from the picker.
+        "log",
+        "node_modules",
+        "tmp",
+      },
       formatters = {
         file = {
           truncate = 1000, -- truncate the file path to (roughly) this length
         },
       },
-      layout = { layout = { backdrop = false } },
+      layout = {
+        layout = {
+          backdrop = false,
+        },
+      },
+      sources = {
+        files = {
+          ignored = true, -- Include ignored files.
+          hidden = true, -- Include hidden files.
+        },
+        grep = {
+          hidden = true, -- Include hidden files.
+          ignored = true, -- Include ignored files.
+        },
+      },
       win = {
         input = {
           keys = {
