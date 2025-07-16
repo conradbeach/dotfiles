dc2a2801693a367b1269237958bdd4b6b697b2ca
Conrad Beach <cbeach@deseretbook.com>
2025-07-16 12:03:58 -0600

    Improve Spectre config
    Refactored the code.
    Added the following mappings:
    - Search for the current word only in the current file.
    - Search only in non-spec files.
    - Search for the current word in only non-spec files.

--- astronvim/lua/plugins/spectre.lua
+++ astronvim/lua/plugins/spectre.lua
@@ -7,21 +7,31 @@ return {
       opts = function(_, opts)
         local maps = opts.mappings
         local prefix = "<Leader>s"
-        local current_word_search = function() require("spectre").open_visual { select_word = true } end
-        local current_word_desc = "Current Word"
-
         maps.n[prefix] = { desc = require("astroui").get_icon("Spectre", 1, true) .. "Search and Replace" }
-        maps.n[prefix .. "s"] = { function() require("spectre").open() end, desc = "Global" }
-        maps.n[prefix .. "f"] = { function() require("spectre").open_file_search() end, desc = "Current File" }
-        maps.n[prefix .. "w"] = {
-          current_word_search,
-          desc = current_word_desc,
+        maps.n[prefix .. "s"] = {
+          function() require("spectre").open() end,
+          desc = "Global",
         }
-
-        maps.x[prefix] = maps.n[prefix]
-        maps.x[prefix .. "w"] = {
-          current_word_search,
-          desc = current_word_desc,
+        maps.n[prefix .. "f"] = {
+          function() require("spectre").open_file_search() end,
+          desc = "Current File",
+        }
+        maps.n[prefix .. "w"] = {
+          function() require("spectre").open_visual({ select_word = true }) end,
+          desc = "Current Word",
+        }
+        maps.n[prefix .. "i"] = {
+          function() require("spectre").open_file_search({ select_word = true }) end,
+          desc = "Current Word in File",
+        }
+        maps.n[prefix .. "n"] = { desc = "Non-Spec Files" }
+        maps.n[prefix .. "nn"] = {
+          function() require("spectre").open({ path = "!spec" }) end,
+          desc = "Non-Spec Files",
+        }
+        maps.n[prefix .. "nw"] = {
+          function() require("spectre").open_visual({ select_word = true, path = "!spec" }) end,
+          desc = "Current Word in Non-Spec Files",
         }
       end,
     },
