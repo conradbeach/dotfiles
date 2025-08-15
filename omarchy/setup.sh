7447761f2335e45ecb121e529533a5fae3b95cdb
Conrad Beach <conradbeach@hey.com>
2025-08-15 07:02:05 -0600

    Further Omarchy config and setup

--- omarchy/setup.sh
+++ omarchy/setup.sh
@@ -1,7 +1,15 @@
 ln -sfn ~/development/dotfiles/omarchy/hypr/monitors.conf ~/.config/hypr/
 ln -sfn ~/development/dotfiles/omarchy/hypr/input.conf ~/.config/hypr/
 ln -sfn ~/development/dotfiles/alacritty.toml ~/.config/alacritty/
+ln -sf ~/development/dotfiles/.gitconfig ~
 
 yay -Sy --noconfirm --needed \
   gitkraken \
   zen-browser-bin
+
+# SSH
+ssh-keygen -t ed25519 -C "conradbeach@hey.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+ln -sf ~/development/dotfiles/omarchy/ssh/config ~/.ssh/
+ln -sf ~/development/dotfiles/ssh/known_hosts ~/.ssh/
