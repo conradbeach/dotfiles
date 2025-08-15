31e0b97470b5b0288216274293f57b22fc10fcc3
Conrad Beach <conradbeach@hey.com>
2025-08-15 06:08:54 -0600

    Add Omarchy setup script

---
+++ omarchy/setup.sh
@@ -0,0 +1,7 @@
+ln -sfn ~/development/dotfiles/omarchy/hypr/monitors.conf ~/.config/hypr/
+ln -sfn ~/development/dotfiles/omarchy/hypr/input.conf ~/.config/hypr/
+ln -sfn ~/development/dotfiles/alacritty.toml ~/.config/alacritty/
+
+yay -Sy --noconfirm --needed \
+  gitkraken \
+  zen-browser-bin
