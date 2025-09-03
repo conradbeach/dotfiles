c4e2b72aedad1ee7bf580ed2d87ec6b5ae9ce6ab
Conrad Beach <cbeach@deseretbook.com>
2025-09-03 10:37:58 -0600

    Add Rubocop todo check to `chma`

--- zsh/functions/chma.zsh
+++ zsh/functions/chma.zsh
@@ -4,14 +4,19 @@ chma() {
   bundle exec rubocop
   rubocop_status=$?
 
+  echo "\n---------- Check for RuboCop violations in changed files with .rubocop_todo.yml disabled ----------"
+  bundle exec rake rubocop:changed_files_todo_disabled
+  rubocop_no_todo_status=$?
+
   echo "\n---------- Running Parallel RSpec ----------"
   bundle exec parallel_rspec
   rspec_status=$?
 
   echo "\n---------- Results ----------"
   report_result "Rubocop" $rubocop_status
+  report_result "Rubocop with Todo Disabled" $rubocop_no_todo_status
   report_result "RSpec" $rspec_status
 
   # Return overall success
-  return $(( rubocop_status + rspec_status ))
+  return $(( rubocop_status + rubocop_no_todo_status + rspec_status ))
 }
